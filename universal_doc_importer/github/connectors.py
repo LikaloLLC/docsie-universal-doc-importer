f494697fae9531c90b399dc7f3d0d1d9de286c9b
zarif99 <9685440@gmail.com>
2021-06-15 17:25:13 +0500

    get files and theirs content

--- universal_doc_importer/github/connectors.py
+++ universal_doc_importer/github/connectors.py
@@ -4,10 +4,12 @@ from urllib.parse import urlparse
 from swag_auth.github.connectors import GithubAPIConnector
 
 from universal_doc_importer.file_system import FileSystem
-from universal_doc_importer.utils import filter_by_extension
+from universal_doc_importer.utils import filter_by_extension, get_repo_content_path
 
 
 class GithubImporter(GithubAPIConnector):
+    provider_id = 'github'
+
     def _parse_url(self, url: str) -> str:
         uri = urlparse(url)
         urls = uri.path
@@ -28,7 +30,7 @@ class GithubImporter(GithubAPIConnector):
 
     def get_repo_map(self, url, extensions):
         repo_name = self._parse_url(url=url)
-        folder_name = repo_name.split('/')[1]
+        folder_name = repo_name.split('/')[0] + '-' + repo_name.split('/')[1]
         repo = self.get_user_repo(repo_name)
 
         contents = repo.get_contents("")
@@ -43,6 +45,15 @@ class GithubImporter(GithubAPIConnector):
             if extension in extensions or file_content.path == head:
                 mapping.addChild(file_content.path)
         return filter_by_extension(mapping.makeDict(), extensions)
+
+    def get_files(self, repo_map):
+        repo_name = list(repo_map)[0]
+        repo_name = repo_name.split('-')[0] + '/' + repo_name.split('-')[1]
+        urls = get_repo_content_path(repo_map)
+        repo = self.get_user_repo(repo_name)
+        for url in urls:
+            content = self.get_swagger_content(repo, url)
+            yield url, content
 
 
 connector_classes = [GithubImporter]
