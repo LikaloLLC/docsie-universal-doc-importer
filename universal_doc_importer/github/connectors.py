1df9344997e579cde8e46e4a77d7fccd730fb345
1mace1 <belyaev.nikit@gmail.com>
2021-06-17 22:08:42 +0300

    Removed old and ugly file system generator and added RepositoryMap class, that is used to create a nice repository mapping

--- universal_doc_importer/github/connectors.py
+++ universal_doc_importer/github/connectors.py
@@ -3,7 +3,7 @@ from urllib.parse import urlparse
 
 from swag_auth.github.connectors import GithubAPIConnector
 
-from universal_doc_importer.file_system import FileSystem
+from universal_doc_importer.repo_map import RepositoryMap
 from universal_doc_importer.utils import get_repo_content_path
 
 
@@ -33,18 +33,18 @@ class GithubImporter(GithubAPIConnector):
         repo = self.get_user_repo(repo_name)
 
         contents = repo.get_contents("")
-        mapping = FileSystem(extensions=extensions, filePath=folder_name)
+        repo_map = RepositoryMap(folder_name, extensions)
         while contents:
             file_content = contents.pop(0)
             if file_content.type == "dir":
                 contents.extend(repo.get_contents(file_content.path))
+            else:
+                extension = os.path.splitext(file_content.path)[1][1:]
 
-            head, extension = os.path.splitext(file_content.path)
-            extension = extension.replace('.', '')
+                if extension in extensions:
+                    repo_map.add_path(file_content.path)
 
-            if extension in extensions or file_content.path == head:
-                mapping.addChild(file_content.path)
-        return mapping.buildDict()
+        return repo_map.as_dict()
 
     def get_files(self, repo_map):
         repo_name = list(repo_map)[0]
