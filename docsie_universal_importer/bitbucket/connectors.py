9cdf5fa355a8e731d99df859802e8b0fd731dbfb
Nick Belyaev <nick@docsie.io>
2021-07-02 21:29:55 +0000

    Added an ImporterMixin, which contains methods for retrieving repo map and getting the files

--- docsie_universal_importer/bitbucket/connectors.py
+++ docsie_universal_importer/bitbucket/connectors.py
@@ -1,47 +1,22 @@
-import os
-
 from swag_auth.bitbucket.connectors import BitbucketSwaggerDownloader
 
-from docsie_universal_importer.repo_map import RepositoryMap
-from docsie_universal_importer.utils import get_repo_content_path
+from docsie_universal_importer.mixin import ImporterMixin
 
 
-class BitbucketImporter(BitbucketSwaggerDownloader):
+class BitbucketImporter(BitbucketSwaggerDownloader, ImporterMixin):
     provider_id = 'bitbucket'
 
-    def get_repo_map(self, url, extensions):
-        owner, repo_name, branch, path = self._parse_url(url=url)
-        repo = self.get_user_repo(f'{owner}/{repo_name}')
+    def get_contents(self, repo, path, branch):
+        return self.get_file_content(repo=repo, path=path, ref=branch)
 
-        if not branch:
-            branch = self.get_default_branch(repo)
+    def get_repo_paths(self, contents, repo, branch):
 
-        contents = self.get_file_content(repo=repo, path=path, ref=branch)
-
-        repo_map = RepositoryMap(repo_name, extensions)
         while contents:
             file_content = contents.pop(0)
             if file_content.get('type') == "commit_directory":
-                contents.extend(self.get_file_content(repo=repo, path=file_content.get('path'), ref=branch))
+                contents.extend(self.get_contents(repo, file_content.get('path'), branch))
             else:
-                extension = os.path.splitext(file_content.get('path'))[1][1:]
-
-                if extension in extensions:
-                    repo_map.add_path(f'{branch}/{file_content.get("path")}')
-
-        return {owner: repo_map.as_dict()}
-
-    def get_files(self, repo_map):
-        owner, repo_name, branch = '', '', ''
-        for key, value in repo_map.items():
-            owner, repo_name, repo_map = key, list(value)[0], value
-            break
-        urls = get_repo_content_path(repo_map)
-        repo = self.get_user_repo(f'{owner}/{repo_name}')
-        for url in urls:
-            branch, url = url.split('/', 1)[0], url.split('/', 1)[1]
-            content = self.get_file_content(repo, url, branch)
-            yield url, content
+                yield f'{branch}/{file_content.get("path")}'
 
 
 connector_classes = [BitbucketImporter]
--- docsie_universal_importer/github/connectors.py
+++ docsie_universal_importer/github/connectors.py
@@ -1,46 +1,21 @@
-import os
-
 from swag_auth.github.connectors import GithubSwaggerDownloader
 
-from docsie_universal_importer.repo_map import RepositoryMap
-from docsie_universal_importer.utils import get_repo_content_path
+from docsie_universal_importer.mixin import ImporterMixin
 
 
-class GithubImporter(GithubSwaggerDownloader):
+class GithubImporter(GithubSwaggerDownloader, ImporterMixin):
     provider_id = 'github'
 
-    def get_repo_map(self, url, extensions):
-        owner, repo_name, branch, path = self._parse_url(url=url)
-        repo = self.get_user_repo(f'{owner}/{repo_name}')
+    def get_contents(self, repo, path, branch):
+        return repo.get_contents(path="", ref=branch)
 
-        if not branch:
-            branch = self.get_default_branch(repo)
-
-        contents = repo.get_contents("", ref=branch)
-        repo_map = RepositoryMap(repo_name, extensions)
+    def get_repo_paths(self, contents, repo, branch):
         while contents:
             file_content = contents.pop(0)
             if file_content.type == "dir":
-                contents.extend(repo.get_contents(path=file_content.path, ref=branch))
+                contents.extend(self.get_contents(repo, file_content.path, branch))
             else:
-                extension = os.path.splitext(file_content.path)[1][1:]
-
-                if extension in extensions:
-                    repo_map.add_path(f'{branch}/{file_content.path}')
-
-        return {owner: repo_map.as_dict()}
-
-    def get_files(self, repo_map):
-        owner, repo_name, branch = '', '', ''
-        for key, value in repo_map.items():
-            owner, repo_name, repo_map = key, list(value)[0], value
-            break
-        urls = get_repo_content_path(repo_map)
-        repo = self.get_user_repo(f'{owner}/{repo_name}')
-        for url in urls:
-            branch, url = url.split('/', 1)[0], url.split('/', 1)[1]
-            content = self.get_file_content(repo, url, branch)
-            yield url, content
+                yield f'{branch}/{file_content.path}'
 
 
 connector_classes = [GithubImporter]
