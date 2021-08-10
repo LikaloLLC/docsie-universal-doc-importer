5a185503ee759b114ed433a0ada5e99f659a23d2
Nick Belyaev <nick@docsie.io>
2021-08-10 13:20:51 +0000

    Removed gitlab/adapter.py module

--- docsie_universal_importer/providers/gitlab/adapter.py
+++
@@ -1,7 +0,0 @@
-from docsie_universal_importer.import_adapter import ImportAdapter
-
-
-class GitlabImportAdapter(ImportAdapter):
-    def import_content(self, file, content):
-        print(file)
-        print(type(content))
