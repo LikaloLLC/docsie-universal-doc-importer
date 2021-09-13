4b59beea6b86dd887ebad0410ce285110aa3408e
1mace1 <belyaev.nikit@gmail.com>
2021-09-13 16:32:31 +0300

    Added ConnectorToken model

--- docsie_universal_importer/models.py
+++ docsie_universal_importer/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class ConnectorToken(models.Model):
+    # Must contain provider id
+    provider = models.CharField(
+        verbose_name=_('provider'),
+        max_length=30,
+    )
+
+    token = models.TextField(
+        verbose_name=_('token'),
+        help_text=_('"oauth_token" (OAuth1) or access token (OAuth2)'),
+    )
+    token_secret = models.TextField(
+        blank=True,
+        verbose_name=_('token secret'),
+        help_text=_('"oauth_token_secret" (OAuth1) or refresh token (OAuth2)'),
+    )
+    expires_at = models.DateTimeField(
+        blank=True, null=True, verbose_name=_('expires at')
+    )
+
+    def __str__(self):
+        return self.token
